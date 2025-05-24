2eb40226081ff052760616f3263bd4f53b1e7df7
Nam <namdeveloper.ca@gmail.com>
2025-05-24 21:12:03 +0700

    Delete public file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function openWhatsAppChat(phoneNumber: string, message?: string) {
-  const encodedMessage = message ? encodeURIComponent(message) : ""
-  const whatsappUrl = `https://wa.me/${phoneNumber}${message ? `?text=${encodedMessage}` : ""}`
-  window.open(whatsappUrl, "_blank")
+  const encodedMessage = message ? encodeURIComponent(message) : ''
+  const whatsappUrl = `https://wa.me/${phoneNumber}${message ? `?text=${encodedMessage}` : ''}`
+  window.open(whatsappUrl, '_blank')
 }
