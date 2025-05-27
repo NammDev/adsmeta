8c7b688cb421179701a94aec7d796634e2722ef0
nammdev <namdeveloper.ca@gmail.com>
2025-05-27 18:44:44 +0700

    avatar config

--- config/avatars.ts
+++ config/avatars.ts
@@ -1,4 +1,8 @@
-export const AVATAR_IMAGES = ["/avatar/alex.png", "avatar/michael.png", "avatar/sarah.png"] as const
+export const AVATAR_IMAGES = [
+  "/avatar/alex.png",
+  "/avatar/michael.png",
+  "/avatar/sarah.png",
+] as const
 
 export function getRandomAvatar() {
   const randomIndex = Math.floor(Math.random() * AVATAR_IMAGES.length)
